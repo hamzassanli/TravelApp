1bca78be9fd34b1ed2fe717f67e4f9bb4d98a6fc
Hamza Şanlı <hamzasanli@Hamza-MacBook-Pro.local>
2025-12-01 12:29:03 +0300

    feat: proje kurulumu ve navigation bağımlılıklarının kurulumu (Bölüm 1)

--- App.js
+++ App.js
@@ -3,12 +3,10 @@ import { Text, View } from'react-native';
 export default function App() {
 return (
 <NavigationContainer>
-<View style={{ flex: 1, justifyContent: 'center'
-'center' }}>
-→
+<View style={{ flex: 1, justifyContent: 'center',alignItems: 'center',
+ }}>
 <Text>Navigation Ready!</Text>
 </View>
 </NavigationContainer>
-, alignItems:
 );
 }
